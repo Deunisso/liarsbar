9c8aa2674116f38fb24409eee03dc1c6d7af6b13
Deunisso <129228213+Deunisso@users.noreply.github.com>
2025-03-29 22:40:43 -0300

    js

--- script/index.js
+++ script/index.js
@@ -5,24 +5,19 @@ function selectPlayer(element, player) {
     const playerSound = new Audio(`./audios/${player}.mp3`);
     const deselectSound = new Audio(`./audios/deselect.mp3`);
 
-    // Pré-carregar os áudios
     selectSound.load();
     playerSound.load();
 
-    // Converte o nome antes de adicionar
     const convertedPlayer = convertName(player);
 
     if (selectedPlayers.includes(convertedPlayer)) {
-        // Toca o áudio de deseleção
         deselectSound.play();
 
         selectedPlayers = selectedPlayers.filter(p => p !== convertedPlayer);
         element.classList.remove("selected");
     } else if (selectedPlayers.length < 4) {
-        // Toca o áudio de seleção
         selectSound.play();
 
-        // Play player's specific sound immediately after select.mp3
         selectSound.onended = function () {
             playerSound.play();
         };
@@ -45,12 +40,11 @@ function convertName(name) {
     const nameMapping = {
         'Kaua': 'Kauã',
         'Leticia': 'Letícia',
-        // Adicione outros nomes que precisem de conversão
     };
-    return nameMapping[name] || name; // Retorna o nome convertido ou o nome original se não houver mapeamento
+    return nameMapping[name] || name; 
 }
 
 function startGame() {
     localStorage.setItem("players", JSON.stringify(selectedPlayers));
-    window.location.href = "game.html";  // Substitua pelo nome correto da página do jogo
+    window.location.href = "game.html"; 
 }
